bdc216414fcd0ee377b57cc847403c1447925c2b
run-after <run.after@protonmail.com>
2020-11-27 12:44:12 -0600

    Basic component layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import './reset.css'
 import './App.css';
+import Home from './Home';
+import Shop from './Shop';
+import Nav from './Nav';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';;
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/shop' component={Shop} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav className='nav-bar'>
+      <h1 className='logo'>Nav</h1>
+      <ul className='nav-links'>
+        <li>Home</li>
+        <li>Shop</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
