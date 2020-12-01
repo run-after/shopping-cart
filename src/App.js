35b57230ded75151d8c02bda701676390d0aee6b
run-after <run.after@protonmail.com>
2020-12-01 14:19:23 -0600

    Add cart page and move state to App.js and allow viewing of cart

--- src/App.js
+++ src/App.js
@@ -3,16 +3,77 @@ import './styles/App.css';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Nav from './components/Nav';
+import Cart from './components/Cart';
+import { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';;
 
 function App() {
+
+  const items = [{
+      name: 'Fire Extinguisher',
+      pic: 'https://images.homedepot-static.com/productImages/920fe75c-471f-4aea-ac1e-9e442fd73d06/svn/kidde-fire-extinguishers-21026946p-64_1000.jpg',
+      cost: 40
+    },
+    {
+      name: 'Dog Bed',
+      pic: 'https://4.bp.blogspot.com/-2Pr3neOoJqk/VYp8zpmqJTI/AAAAAAAABzo/d3k6lNNi2qw/s1600/pet%2Bbed.jpg',
+      cost: 18
+    },
+    {
+      name: 'Tool Bag',
+      pic: 'https://www.kleintools.com/sites/all/product_assets/catalog_imagery/klein/5541718_14_alt1.jpg',
+      cost: 80
+    },
+    {
+      name: 'Tinfoil Hat',
+      pic: 'https://i2.wp.com/media.boingboing.net/wp-content/uploads/2018/03/Tin-Foil-Hat-for-Humans-by-Archie-McPhee.jpg?fit=600,600&ssl=1',
+      cost: 10
+    }
+  ];
+  
+  const [cartQty, setCartQty] = useState(0);
+
+  const [cart, setCart] = useState({});
+
+  const addToCart = (e) => {
+    const itemID = e.target.parentNode.parentNode.id;
+    // Number from input
+    const number = Number(e.target.parentNode.firstChild.value);
+    if (!(items[itemID].name in cart)) {
+      setCart(prevState => ({
+        ...prevState,
+        [items[itemID].name]: {
+          name: items[itemID].name,
+          pic: items[itemID].pic,
+          cost: items[itemID].cost,
+          qty: number
+        }
+      }));  
+    } else {
+      cart[items[itemID].name].qty += number;
+    };
+
+    setCartQty(cartQty + number);
+    
+    e.target.parentNode.firstChild.value = 1;
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route path='/shop' component={Shop} />
+          <Route path='/shop'
+            render={(props) =>
+              <Shop {...props}
+                items={items}
+                cart={cart}
+                cartQty={cartQty}
+                addToCart={addToCart}
+              />}
+          />
+          <Route path='/cart' component={Cart} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,58 +1,16 @@
 import React from 'react';
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Shop = () => {
-
-  const [cartQty, setCartQty] = useState(0);
-
-  const [cart, setCart] = useState([]);
-
-  const items = [{
-    name: 'Fire Extinguisher',
-    pic: 'https://images.homedepot-static.com/productImages/920fe75c-471f-4aea-ac1e-9e442fd73d06/svn/kidde-fire-extinguishers-21026946p-64_1000.jpg',
-    cost: 40,
-    qty: 0
-    },
-    {
-      name: 'Dog Bed',
-      pic: 'https://4.bp.blogspot.com/-2Pr3neOoJqk/VYp8zpmqJTI/AAAAAAAABzo/d3k6lNNi2qw/s1600/pet%2Bbed.jpg',
-      cost: 18,
-      qty: 0
-    },
-    {
-      name: 'Tool Bag',
-      pic: 'https://www.kleintools.com/sites/all/product_assets/catalog_imagery/klein/5541718_14_alt1.jpg',
-      cost: 80,
-      qty: 0
-    },
-    {
-      name: 'Tinfoil Hat',
-      pic: 'https://i2.wp.com/media.boingboing.net/wp-content/uploads/2018/03/Tin-Foil-Hat-for-Humans-by-Archie-McPhee.jpg?fit=600,600&ssl=1',
-      cost: 10,
-      qty: 0
-    }
-  ];
-
-  const addToCart = (e) => {
-    const itemID = e.target.parentNode.parentNode.id;
-    // Number from input
-    const number = Number(e.target.parentNode.firstChild.value); 
-    items[itemID].qty = number;
-    setCart(cart.concat(items[itemID]));
-    
-    setCartQty(cartQty + number);
-    e.target.parentNode.firstChild.value = 1;
-  };
-
+const Shop = (props) => {
   return (
     <div>
       <div className='cart'>
-        Items: {cartQty}
+        <Link to={{pathname: '/cart', cart: props.cart}}>Items: {props.cartQty}</Link>
         <button onClick={()=> alert('This is where we would take you to the checkout screen') }className='checkout'>Checkout</button>
       </div>
       <h1>Shop</h1>
       <div className='store'>
-        {items.map((x, i) => {
+        {props.items.map((x, i) => {
           return (
             <div id={i} key={x.name} className='item-card'>
               {x.name}<br/>
@@ -60,7 +18,7 @@ const Shop = () => {
               <img src={x.pic} alt={x.name} />
               <div>
                 <input type='number' defaultValue='1' />
-                <button onClick={addToCart}>Add to cart</button>
+                <button onClick={props.addToCart}>Add to cart</button>
               </div>
             </div>
           );
